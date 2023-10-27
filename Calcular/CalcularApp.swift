60fc6fd9563b61c4bfa3c719824498c70c4863e1
goncalveshigino <goncalveshigino20@gmail.com>
2023-10-27 08:21:46 +0100

    Initial Commit

---
+++ Calcular/CalcularApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcularApp.swift
+//  Calcular
+//
+//  Created by Goncalves Higino on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CalcularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
